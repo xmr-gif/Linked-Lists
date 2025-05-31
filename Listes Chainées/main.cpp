e77219509f9d8f31f6d536ea5e2156d2dcb49b73
OUSSAMA <hafidoss2004@gmail.com>
2025-05-31 23:17:50 +0100

    Update main.cpp

--- Listes Chainées/main.cpp
+++ Listes Chainées/main.cpp
@@ -1,7 +1,22 @@
+#include "Liste.h"
 #include <iostream>
-#include "Liste.hpp"
 
-int main() {
+using namespace std;
+
+int main()
+{
+    Liste A ;
+    A.afficher() ;
+    A.ins_debut_liste("Hello");
+    A.ins_debut_liste("World") ;
+    A.afficher() ;
+    A.supp_debut();
+    A.afficher() ;
+    Liste B(A) ; // appel de constructeur de recopie 
     
+    Liste C  ; // appel de constructeur par défaut 
+    
+    C = A ; // appel d'op d'affectaion 
+
     return 0;
 }
